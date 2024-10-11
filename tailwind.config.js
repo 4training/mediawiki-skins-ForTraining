d680f9007e4657dd12d030d3138a8d69464e7b9a
Christopher Hogg <cthogg@users.noreply.github.com>
2024-10-11 23:04:25 +0100

    feat: working

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./skins/**/*.{php,html,js,mustache}",
+    "./resources/**/*.{php,html,js,mustache}",
+    "./extensions/**/*.{php,html,js,mustache}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

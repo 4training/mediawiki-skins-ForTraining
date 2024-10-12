45c6228aba0659056ace95d14fe0ee7056548cda
Christopher Hogg <cthogg@users.noreply.github.com>
2024-10-12 17:40:16 +0100

    feat: remove things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./templates/**/*.{php,html,js,mustache}",
-    "./resources/**/*.{php,html,js,mustache}",
-    "./extensions/**/*.{php,html,js,mustache}",
-  ],
+  content: ["./templates/**/*.{php,html,js,mustache}"],
   theme: {
     extend: {},
   },

737fc06cbce33a551cc151101b39448aee0161c1
Christopher Hogg <cthogg@users.noreply.github.com>
2024-10-12 12:38:02 +0100

    feat: add onlclick

--- resources/tailwindburger.js
+++ resources/tailwindburger.js
@@ -2,12 +2,34 @@ mw.loader.using("mediawiki.util").then(function () {
   console.log("tailwindburger.js loaded");
 
   $(function () {
-    var $mobileMenuTrigger = $("#mobile-menu-trigger");
+    // Burger menus
+    const burger = document.querySelectorAll(".navbar-burger");
+    const menu = document.querySelectorAll(".navbar-menu");
+    const close = document.querySelectorAll(".navbar-close");
+    const backdrop = document.querySelectorAll(".navbar-backdrop");
 
-    if ($mobileMenuTrigger.length) {
-      $mobileMenuTrigger.on("click", function () {
-        alert("hello");
-      });
+    function toggleMenu() {
+      for (var j = 0; j < menu.length; j++) {
+        menu[j].classList.toggle("hidden");
+      }
+    }
+
+    if (burger.length && menu.length) {
+      for (var i = 0; i < burger.length; i++) {
+        burger[i].addEventListener("click", toggleMenu);
+      }
+    }
+
+    if (close.length) {
+      for (var i = 0; i < close.length; i++) {
+        close[i].addEventListener("click", toggleMenu);
+      }
+    }
+
+    if (backdrop.length) {
+      for (var i = 0; i < backdrop.length; i++) {
+        backdrop[i].addEventListener("click", toggleMenu);
+      }
     }
   });
 });
